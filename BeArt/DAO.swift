f8e0c5844b44e40007293cdcedc17ae81d173586
alenamiklos <alenamiklos@Alenas-MacBook-Pro-2.local>
2015-05-21 18:07:15 -0300

    plist

---
+++ BeArt/DAO.swift
@@ -0,0 +1,15 @@
+//
+//  DAO.swift
+//  BeArt
+//
+//  Created by Alena Miklos on 21/05/15.
+//  Copyright (c) 2015 BEPiD. All rights reserved.
+//
+
+import Foundation
+
+class DAO
+
+{
+    //PROPRIEDADES E METODOS
+}
